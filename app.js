8a8d05269d76eb0a4a5af86e56a743d8f5540136
Tareq Zeadeh <tareq.ha130@yahoo.com>
2021-05-27 15:32:02 +0300

    problem2 solved

--- app.js
+++ app.js
@@ -30,12 +30,13 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) {
   //eslint-disable-line
-
+  let answer=a*b;
+  return[answer,`The product of ${a} and ${b} is ${answer}.`]
   
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -83,16 +84,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4]; 
 
 function sumArray(sumArr) {
 
-  //eslint-disable-line
+  let answer = 0;
+  let sum2=0;
+  sum2=sum(sumArr[0],sumArr[1])[0];
+  answer = sum(sum2,sumArr[2])[0];
+  return[answer,`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${answer} is their sum.`] 
 }
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
